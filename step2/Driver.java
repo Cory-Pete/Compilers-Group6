baf719a1869a7d0dbdace620f787b7b901cfd8f5
Hughman25 <hughman25@gmail.com>
2019-02-26 16:49:12 -0700

    Updated Driver w/ ParseTreeWalker + .gitignore

--- step2/Driver.java
+++ step2/Driver.java
@@ -6,25 +6,22 @@ import org.antlr.v4.runtime.*;
 public class Driver {
 	public static void main(String[] args) throws IOException {
 		PrintWriter writer = new PrintWriter("group6.output", "UTF-8"); //this is used to print to output file
+		String returnS;
 		// LittleLexer lexer = null;
 		File input = new File(args[0]);
 		LittleLexer lexer = new LittleLexer(CharStreams.fromFileName(args[0]));
 		CommonTokenStream tokenStream = new CommonTokenStream(lexer);
 		// LittleLexer lexer = new LittleLexer(CharStreams.fromFileName("../Step1/inputs/sqrt.micro"));
 		LittleParser parser = new LittleParser(tokenStream);
-		parser.program();
-		
-
-		// Vocabulary voc = lexer.getVocabulary();
-		// //tok.getType != EOF
-		// Token token = lexer.nextToken();
-		// while(token.getType() != token.EOF) {
-		// 	System.out.println("Token Type: " + voc.getSymbolicName(token.getType()));
-		// 	writer.write("Token Type: " + voc.getSymbolicName(token.getType()) + "\n");
-		// 	System.out.println("Value: " + token.getText());
-		// 	writer.write("Value: " + token.getText() + "\n");
-		// 	token = lexer.nextToken();
-		// }	
+		ParseTreeWalker walker = new ParseTreeWalker();
+		walker.walk(new ParseTreeListener(), parser.program());
+		if(parser.getNumberOfSyntaxErrors() > 0)
+		{
+			returnS = "Accepted";
+		}
+		else{
+			returnS = "Not Accepted";
+		}
 		writer.close();
 	}
 }
