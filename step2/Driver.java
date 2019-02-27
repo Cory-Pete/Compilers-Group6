265c33691f8c9b55f8631f0234ac71a2e987621c
Hughman25 <hughman25@gmail.com>
2019-02-26 17:06:34 -0700

    Finished Driver.java, Little.g4 needs work

--- step2/Driver.java
+++ step2/Driver.java
@@ -1,27 +1,23 @@
 import java.io.IOException;
-import java.io.File;
-import java.io.PrintWriter;
 import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
 
 public class Driver {
 	public static void main(String[] args) throws IOException {
-		PrintWriter writer = new PrintWriter("group6.output", "UTF-8"); //this is used to print to output file
 		String returnS;
-		// LittleLexer lexer = null;
-		File input = new File(args[0]);
 		LittleLexer lexer = new LittleLexer(CharStreams.fromFileName(args[0]));
 		CommonTokenStream tokenStream = new CommonTokenStream(lexer);
-		// LittleLexer lexer = new LittleLexer(CharStreams.fromFileName("../Step1/inputs/sqrt.micro"));
 		LittleParser parser = new LittleParser(tokenStream);
 		ParseTreeWalker walker = new ParseTreeWalker();
-		walker.walk(new ParseTreeListener(), parser.program());
+		LittleBaseListener listener = new LittleBaseListener();
+		walker.walk(listener, parser.program());
 		if(parser.getNumberOfSyntaxErrors() > 0)
 		{
-			returnS = "Accepted";
-		}
-		else{
 			returnS = "Not Accepted";
 		}
-		writer.close();
+		else{
+			returnS = "Accepted";
+		}
+		System.out.println(returnS);
 	}
-}
+}      
