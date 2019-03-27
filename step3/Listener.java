b1445ddc6a31f0a5e69f9338bd555ab289fb1036
Hughman25 <hughman25@gmail.com>
2019-03-27 16:17:29 -0600

    Added listener class

---
+++ step3/Listener.java
@@ -0,0 +1,17 @@
+
+class Listener extends LittleBaseListener
+{
+    SymbolTable st = new SymbolTable();
+
+    @Override
+    public void enterFunc_decl(LittleParser.Func_declContext ctx)
+    {
+        //operate on symbol table here
+    }
+
+    @Override
+    public void exitFunc_decl(LittleParser.Func_declContext ctx)
+    {
+        //operate on symbol table here
+    }
+}
