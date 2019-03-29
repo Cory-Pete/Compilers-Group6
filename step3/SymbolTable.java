d7688933c52ad4549bf424528fd9966bed696d0c
Matthew Sagen <matthewsagen@Matthews-MacBook-Pro.local>
2019-03-28 18:06:42 -0600

    c

--- step3/SymbolTable.java
+++ step3/SymbolTable.java
@@ -1,13 +1,21 @@
 import java.util.*;
+
 public class SymbolTable {
 
     ArrayList<LittleParser.Assign_exprContext> list = new ArrayList<LittleParser.Assign_exprContext>();
-    public void addScope() {
+    Stack<LittleParser.Func_declContext> stack = new Stack<LittleParser.Func_declContext>();
+
+    public void addScope(LittleParser.Func_declContext ctx) {
+        stack.push(ctx);
     }
 
+    public void popStack(){
+        stack.pop();
+    }
+    
     public void addSymbol() {
-    }
 
+    }
     public void add(LittleParser.Assign_exprContext ctx){
         list.add(ctx);
     }
