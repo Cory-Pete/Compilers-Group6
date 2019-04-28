12999c692d13b64289563574b77df164c614a7ab
Hughman25 <hughman25@gmail.com>
2019-04-27 19:11:35 -0600

    Added some IR generation

--- step4/MakeIRList.java
+++ step4/MakeIRList.java
@@ -5,8 +5,12 @@ import java.util.*;
 
 public class MakeIRList {
     private static int registers = 0;
+    private ArrayList<IRNode> irlist = new ArrayList<IRNode>();
 
     public static String traverse(ASTNode n){
+        String leftside = "";
+        String rightside = "";
+        boolean flip = false;
         if(n.leftChild == null && n.rightChild == null)
         {
             return n.data;
@@ -17,13 +21,52 @@ public class MakeIRList {
         }
         if(n.operator != null)
         {
+            rightSide = generateCode(child.rightChild);
+            leftSide = generateCode(child.rightChild);
+
             if(child.rightChild.type == "INT"){
                 flip = true;
             }
             else{
                 flip = false;
             }
+
+            if(flip){
+                switch(operator){
+                    case "+":
+                        threeAC = ";ADDI " + leftSide + " " + rightSide + " " + "$T" + register + "\n";
+                        break;
+                    case "-":
+                        threeAC = ";SUBI " + leftSide + " " + rightSide + " " + "$T" + register + "\n";
+                        break;
+                    case "*":
+                        threeAC = ";MULI " + leftSide + " " + rightSide + " " + "$T" + register + "\n";
+                        break;
+                    case "/":
+                        threeAC = ";DIVI " + leftSide + " " + rightSide + " " + "$T" + register + "\n";
+                        break;
+                }
+            }
+            else{
+                switch(operator){
+                    case "+":
+                        threeAC = ";ADDF " + leftSide + " " + rightSide + " " + "$T" + register + "\n";
+                        break;
+                    case "-":
+                        threeAC = ";SUBF " + leftSide + " " + rightSide + " " + "$T" + register + "\n";
+                        break;
+                    case "*":
+                        threeAC = ";MULF " + leftSide + " " + rightSide + " " + "$T" + register + "\n";
+                        break;
+                    case "/":
+                        threeAC = ";DIVF " + leftSide + " " + rightSide + " " + "$T" + register + "\n";
+                        break;
+                }
+            }
+
+            //make new IRNode
         }
-        return "";
+        register += 1;
+        return str(register-1);
     }
 }
