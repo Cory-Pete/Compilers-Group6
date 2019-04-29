d1586b3f610990d27599e151a28566e480490a6a
Matthew Sagen <matthewsagen@Matthews-MacBook-Pro-2.local>
2019-04-28 21:36:21 -0600

    ayo

--- step4/new_Tiny.c
+++ step4/new_Tiny.c
@@ -3,12 +3,14 @@ label main
 move 0 i
 move 0 a
 move 10 b
-addi a 1 3
-move 3 a
-addi b 1 5
-move 5 b
-addi i 1 7
-move 7 i
+move 10 3
+move 10 4
+addi a 1 5
+move 5 a
+addi b 1 7
+move 7 b
+addi i 1 9
+move 9 i
 sys writei a
 sys writes newline
 sys writei b
